e176cce9664eeabba9145b8ec3fabae1b975462d
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-08 23:25:59 -0500

    Update P1_CharForLoopOutput.java

--- src/P1_CharForLoopOutput.java
+++ src/P1_CharForLoopOutput.java
@@ -1,23 +1,26 @@
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s):Clayton Bruce
+	Date: 4/8/2018
 */
 
 /*
  Your expected results after the input is provided:
  
- 1.
- 2.
- 3. 
- 4.
+ 1. efg
+ 2. nopqrs
+ 3. should ouput to be nothing. Z-a is not printable
+ 4. qr
   
  */
 public class P1_CharForLoopOutput {
 
 	public static void main(String[] args) {
 		// put your call to printRange below here
-
+		printRange('e', 'g');
+		printRange('n', 's');
+	    printRange('z', 'a');
+		printRange('q', 'r');
 	}
 	
 	public static void printRange(char startLetter, char endLetter ) {
